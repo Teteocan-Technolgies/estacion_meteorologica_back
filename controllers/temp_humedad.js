8f6dcfeb6d077ac3bbfadff0d439d771cc8c95cc
JuanVazquez69 <47.juanantonio1d@gmail.com>
2025-06-26 16:42:50 -0600

    Added /api to routers and added a route to get a group each minute.

--- controllers/temp_humedad.js
+++ controllers/temp_humedad.js
@@ -3,12 +3,18 @@ import { TempHumedadModel } from "../models/local-files/temp_humedad.js";
 export class TempHumedadController {
     static async getAll(req, res){
         const temp_humedad = await TempHumedadModel.getAll();
-        res.json(temp_humedad);
+        return res.json(temp_humedad);
     }
 
     static async getAllByDate(req, res){
         const group_temp_humedad = await TempHumedadModel.getAllByDate(req.body)
         if(group_temp_humedad) return res.json(group_temp_humedad);
-        res.status(404).json({ message: 'No se encontraron datos para ese rango de fechas.' });
+        return res.status(404).json({ message: 'No se encontraron datos para ese rango de fechas.' });
+    }
+
+    static async getOneGroup(req, res){
+        const data_temp_humedad = await TempHumedadModel.getOneGroup()
+        if(data_temp_humedad) return res.json(data_temp_humedad);
+        return res.status(404).json({ message: 'No se encontraron datos para mostrar.' });
     }
 }
