68ebe4ef1bc8c5d953a14ff491dab51ac59f9fbf
EduardoVU <eduardo.ur97@gmail.com>
2025-06-26 23:45:04 -0700

    Feat: changing get to post

--- routes/temp_humedad.js
+++ routes/temp_humedad.js
@@ -4,5 +4,5 @@ import { TempHumedadController } from "../controllers/temp_humedad.js";
 export const temp_humedadRouter = Router();
 
 temp_humedadRouter.get('/', TempHumedadController.getAll)
-temp_humedadRouter.get('/range', TempHumedadController.getAllByDate)
+temp_humedadRouter.post('/range', TempHumedadController.getAllByDate)
 temp_humedadRouter.get('/one', TempHumedadController.getOneGroup)
